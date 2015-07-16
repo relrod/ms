bb6765cceffbe91d20cd7e61c3fa1733f8fb9f73
Ricky Elrod <ricky@elrod.me>
2015-07-15 20:56:08 -0400

    Holy hell this is bad
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Math/MetricSpace.hs
+++ src/Math/MetricSpace.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RankNTypes #-}
 
 module Math.MetricSpace where
@@ -18,32 +19,71 @@ class MetricSpace a where
   (<->) :: Floating b => a -> a -> b
   (<->) = dist
 
-newtype WrappedIntegral a = WrappedIntegral { getA :: Integral a => a }
-newtype Discrete a = Discrete { getDiscrete :: Eq a => a }
+newtype Euclidean1 a = Euclidean1 { getEuclidean1 :: Integral a => a }
+newtype Euclidean2 a b =
+  Euclidean2 { getEuclidean2 :: (Integral a, Integral b) => (a, b) }
+newtype Euclidean3 a b c =
+  Euclidean3 {
+    getEuclidean3 :: (Integral a, Integral b, Integral c) => (a, b, c) }
+newtype Euclidean4 a b c d =
+  Euclidean4 {
+    getEuclidean4 ::
+      (Integral a, Integral b, Integral c, Integral d) => (a, b, c, d) }
+newtype Euclidean5 a b c d e =
+  Euclidean5 {
+    getEuclidean5 ::
+      (Integral a, Integral b, Integral c, Integral d, Integral e) =>
+      (a, b, c, d, e) }
 
--- | The discrete metric.
---
--- @forall a b. if a == b then 0 else 1@
-instance Eq a => MetricSpace (Discrete a) where
-  dist (Discrete a) (Discrete b)
-    | a == b    = 0
-    | otherwise = 1
+newtype Discrete1 a = Discrete1 { getDiscrete1 :: a } deriving (Eq)
+newtype Discrete2 a b =
+  Discrete2 { getDiscrete2 :: (a, b) } deriving (Eq)
+newtype Discrete3 a b c =
+  Discrete3 {
+    getDiscrete3 :: (a, b, c) } deriving (Eq)
+newtype Discrete4 a b c d =
+  Discrete4 {
+    getDiscrete4 :: (a, b, c, d) } deriving (Eq)
+newtype Discrete5 a b c d e =
+  Discrete5 {
+    getDiscrete5 :: (a, b, c, d, e) } deriving (Eq)
+
+-- | 1-dimensional discrete distance.
+instance (Eq a) => MetricSpace (Discrete1 a) where
+  dist a b = if a == b then 0 else 1
+
+-- | 2-dimensional discrete distance.
+instance (Eq a, Eq b) => MetricSpace (Discrete2 a b) where
+  dist a b = if a == b then 0 else 1
+
+-- | 3-dimensional discrete distance.
+instance (Eq a, Eq b, Eq c) => MetricSpace (Discrete3 a b c) where
+  dist a b = if a == b then 0 else 1
+
+-- | 4-dimensional discrete distance.
+instance (Eq a, Eq b, Eq c, Eq d) => MetricSpace (Discrete4 a b c d) where
+  dist a b = if a == b then 0 else 1
+
+-- | 5-dimensional discrete distance.
+instance (Eq a, Eq b, Eq c, Eq d, Eq e) =>
+         MetricSpace (Discrete5 a b c d e) where
+  dist a b = if a == b then 0 else 1
 
 -- | 1-dimensional euclidean distance.
-instance Integral a => MetricSpace (WrappedIntegral a) where
-  dist (WrappedIntegral a) (WrappedIntegral b) =
+instance Integral a => MetricSpace (Euclidean1 a) where
+  dist (Euclidean1 a) (Euclidean1 b) =
     sqrt ((fromIntegral a - fromIntegral b)^2)
 
 -- | 2-dimensional euclidean distance.
-instance (Integral a, Integral b) => MetricSpace (a, b) where
-  dist (a, b) (c, d) =
+instance (Integral a, Integral b) => MetricSpace (Euclidean2 a b) where
+  dist (Euclidean2 (a, b)) (Euclidean2 (c, d)) =
     sqrt (
       (fromIntegral a - fromIntegral c)^2 +
       (fromIntegral b - fromIntegral d)^2)
 
 -- | 3-dimensional euclidean distance.
-instance (Integral a, Integral b, Integral c) => MetricSpace (a, b, c) where
-  dist (a, b, c) (d, e, f) =
+instance (Integral a, Integral b, Integral c) => MetricSpace (Euclidean3 a b c) where
+  dist (Euclidean3 (a, b, c)) (Euclidean3 (d, e, f)) =
     sqrt (
       (fromIntegral a - fromIntegral d)^2 +
       (fromIntegral b - fromIntegral e)^2 +
@@ -54,8 +94,8 @@ instance (
   Integral a,
   Integral b,
   Integral c,
-  Integral d) => MetricSpace (a, b, c, d) where
-  dist (a, b, c, d) (e, f, g, h) =
+  Integral d) => MetricSpace (Euclidean4 a b c d) where
+  dist (Euclidean4 (a, b, c, d)) (Euclidean4 (e, f, g, h)) =
     sqrt (
       (fromIntegral a - fromIntegral e)^2 +
       (fromIntegral b - fromIntegral f)^2 +
