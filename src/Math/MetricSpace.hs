c13cb0b447117d34981e7e170e62861655f41f82
Ricky Elrod <ricky@elrod.me>
2015-07-18 15:58:42 -0400

    try a different approach
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Math/MetricSpace.hs
+++ src/Math/MetricSpace.hs
@@ -1,10 +1,9 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE RankNTypes #-}
 
 module Math.MetricSpace where
 
+import Data.Profunctor
 import qualified Data.Vector as V
 import Text.EditDistance
 
@@ -16,35 +15,35 @@ import Text.EditDistance
 --   (2) __identity of indiscernibles__: @forall x y. 'dist' x y == 0 \<=\> x == y@
 --   (3) __symmetry__: @forall x y. dist x y == 'dist' y x@
 --   (4) __triangle inequality__: @forall x y z. 'dist' x z <= 'dist' x y + 'dist'1 y z@
-class MetricSpace a where
-  dist :: a -> a -> Double
+newtype MetricSpace a b = MetricSpace { dist :: a -> a -> b }
 
-  infix 0 <->
-  -- | Infix notation for 'dist'.
-  (<->) :: a -> a -> Double
-  (<->) = dist
+-- TODO: How to make this useful? Right now the precedence is all wrong.
+infixl 8 <->
+(<->) :: MetricSpace a b -> a -> a -> b
+(<->) = dist
 
--- | Euclidean distance
-data EuclideanVector a where
-  EuclideanVector :: Floating a => V.Vector a -> EuclideanVector a
+instance Profunctor MetricSpace where
+  lmap f (MetricSpace b) = MetricSpace (\x y -> b (f x) (f y))
+  {-# INLINE lmap #-}
 
--- | Discrete distance
-newtype DiscreteVector a = DiscreteVector (V.Vector a) deriving (Eq)
-
--- | Levenshtein distance
-newtype Levenshtein = Levenshtein String
+  rmap f (MetricSpace b) = MetricSpace (\x y -> f (b x y))
+  {-# INLINE rmap #-}
 
 -- | Levenshtein distance between 'String's.
-instance MetricSpace Levenshtein where
-  dist (Levenshtein a) (Levenshtein b) =
-    fromIntegral $ levenshteinDistance defaultEditCosts a b
+levenshtein :: Floating b => MetricSpace String b
+levenshtein =
+  MetricSpace (\a b -> fromIntegral $ levenshteinDistance defaultEditCosts a b)
+{-# INLINE levenshtein #-}
 
 -- | Discrete distance over n-dimensional 'Vector's.
-instance Eq a => MetricSpace (DiscreteVector a) where
-  dist a b = if a == b then 0 else 1
+discrete :: (Eq a, Floating a, Floating b) => MetricSpace (V.Vector a) b
+discrete = MetricSpace (\a b -> if a == b then 0 else 1)
+{-# INLINE discrete #-}
 
 -- | Euclidean distance over n-dimensional 'Vector's.
--- TODO: I want this to be way more general.
-instance MetricSpace (EuclideanVector Double) where
-  dist (EuclideanVector a) (EuclideanVector b) =  f `seq` sqrt f
-    where f = V.sum (V.zipWith (\x y -> (x-y)^2) a b)
+-- TODO: Can we be more general here?
+euclidean :: MetricSpace (V.Vector Double) Double
+euclidean = MetricSpace (\a b -> (f a b) `seq` sqrt (f a b))
+  where
+    f a b = V.sum (V.zipWith (\x y -> (x-y)^2) a b)
+{-# INLINE euclidean #-}
