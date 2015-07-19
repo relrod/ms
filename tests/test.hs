b3eee2c4c906f69850a1ea4835b364b2877a668b
Ricky Elrod <ricky@elrod.me>
2015-07-19 01:17:54 -0400

    This all works but (rightly) fails the triangle test
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- tests/test.hs
+++ tests/test.hs
@@ -1,13 +1,15 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 import Data.Profunctor
+import qualified Data.Vector as V
 import Math.MetricSpace
 import Test.Tasty
 import Test.Tasty.QuickCheck as QC
 
-import qualified Data.Vector as V
+data SameLengthVector3 a = SameLengthVector3 (V.Vector a) (V.Vector a) (V.Vector a) deriving Show
 
 main = defaultMain $ opts $ tests
   where
-    opts = localOption $ QuickCheckTests 3000
+    opts = localOption (QuickCheckTests 3000)
 
 tests :: TestTree
 tests = testGroup "Tests" [properties]
@@ -30,6 +32,13 @@ triangle m a b c = (dist m a c) <= (dist m a b) + (dist m b c)
 instance (Arbitrary a) => Arbitrary (V.Vector a) where
   arbitrary = fmap V.fromList arbitrary
 
+triangleVect
+  :: (Num b, Ord b, Show a, Arbitrary a) =>
+     MetricSpace (V.Vector a) b -> V.Vector a -> Property
+triangleVect m xs =
+  forAll (vectorOf (V.length xs) arbitrary) $ \ys ->
+  forAll (vectorOf (V.length xs) arbitrary) $ \zs -> triangle m xs (V.fromList ys) (V.fromList zs)
+
 genTestGroup
   :: (Num b, Ord b, Show a, Arbitrary a, Eq a) =>
      TestName -> MetricSpace a b -> TestTree
@@ -38,7 +47,20 @@ genTestGroup name metric =
         QC.testProperty "nonnegative" $ \a b -> nonnegative metric a b
       , QC.testProperty "indisc" $ \a -> indisc metric a
       , QC.testProperty "symmetry" $ \a b -> symmetry metric a b
-      --, QC.testProperty "triangle" $ \a b -> triangle metric a b
+      , QC.testProperty "triangle" $ \a b -> triangle metric a b
+      , QC.testProperty "profunctor lmap" $ \a b -> dist (lmap id metric) a b == dist metric a b
+      , QC.testProperty "profunctor rmap" $ \a b -> dist (rmap id metric) a b == dist metric a b
+    ]
+
+genTestGroupV
+  :: (Num b, Ord b, Show a, Arbitrary a, Eq a) =>
+     TestName -> MetricSpace (V.Vector a) b -> TestTree
+genTestGroupV name metric =
+  testGroup name [
+        QC.testProperty "nonnegative" $ \a b -> nonnegative metric a b
+      , QC.testProperty "indisc" $ \a -> indisc metric a
+      , QC.testProperty "symmetry" $ \a b -> symmetry metric a b
+      , QC.testProperty "triangle" $ triangleVect metric
       , QC.testProperty "profunctor lmap" $ \a b -> dist (lmap id metric) a b == dist metric a b
       , QC.testProperty "profunctor rmap" $ \a b -> dist (rmap id metric) a b == dist metric a b
     ]
@@ -47,6 +69,6 @@ qcProps = testGroup "(checked by QuickCheck)"
   [
     genTestGroup "levenshtein" levenshtein
   --, genTestGroup "discrete" discrete
-  , genTestGroup "euclidean" euclidean
-  , genTestGroup "taxicab" taxicab
+  , genTestGroupV "euclidean" euclidean
+  , genTestGroupV "taxicab" taxicab
   ]
